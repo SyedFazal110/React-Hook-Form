fd33e0b5f01abc2353fbbf5846167b504da26637
SyedFazal110 <syedfazal808@gmail.com>
2024-09-23 00:18:46 +0500

    Pending

---
+++ vite-project/src/confin firebase/firebaseconfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2QUjxZK_WTcuiM6lv-s6IvkZwRXNZpx8",
+  authDomain: "react-hook-form-5c453.firebaseapp.com",
+  projectId: "react-hook-form-5c453",
+  storageBucket: "react-hook-form-5c453.appspot.com",
+  messagingSenderId: "613071572660",
+  appId: "1:613071572660:web:7759214e02b88fae5391e1",
+  measurementId: "G-JL7F437ZKF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
